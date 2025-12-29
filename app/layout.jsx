69cb7834ac964202e148b445b4a73471d37137ef
Sarim <dev@Devs-MacBook-Pro-8.local>
2025-12-29 18:49:36 +0500

    Integrated tailwind with next project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="text-4xl font-black text-indigo-600 tracking-tight">
         {children}
       </body>
     </html>
